86af42645d2d1cc911689ad0847aeeb0463b83a9
bharathkrshetty <65807975+bharathkrshetty@users.noreply.github.com>
2023-02-20 11:39:04 +0000

    added the code

--- src/02 Identify a Palindrome/palindrome.py
+++ src/02 Identify a Palindrome/palindrome.py
@@ -1,9 +1,18 @@
 import re
 
+
 def is_palindrome(phrase):
-    forwards = ''.join(re.findall(r'[a-z]+', phrase.lower()))
-    backwards = forwards[::-1]
-    return forwards == backwards
+    phrase1 = phrase.replace(" ", "")
+    phrase2 = ""
+    for char in phrase1:
+        if (char.isalnum()):
+            phrase2 = phrase2+char
+            phrase2 = phrase2.lower()
+    print(phrase2)
+    for i in range(0, int(len(phrase2)/2)):
+        if phrase2[i] != phrase2[len(phrase2)-i-1]:
+            return False
+        return True
 
 
 # commands used in solution video for reference
